9114addbe1fb828a3ef21000b577c31613225f8f
Benjamin Bolte <ben@bolte.cc>
2025-09-18 14:43:33 -0700

    stuff

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,26 +8,6 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Function to confirm with user before taking an action
-confirm_action() {
-    if [ "$#" -ne 2 ]; then
-        echo "Usage: confirm_action <prompt> <action>"
-        return 1
-    fi
-
-    local prompt="$1"
-    local action="$2"
-
-    if read -p "$prompt (y/n) " -n 1 -r; then
-        if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
-            eval "$action"
-            return 0
-        fi
-    fi
-    echo
-    return 1
-}
-
 # ------------------------------------------------
 # Kill USB autosuspend and CPI low-power dithering
 
@@ -38,6 +18,10 @@ fi
 
 for device in /sys/bus/usb/devices/*/power/control; do
     if [ "$(cat $device)" != "on" ]; then
-        confirm_action "Set $device to on?" "echo 'on' > $device"
+        if read -p "Set $device to on? (y/n) " -n 1 -r; then
+            if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
+                echo "on" > $device
+            fi
+        fi
     fi
 done
