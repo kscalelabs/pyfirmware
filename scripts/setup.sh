ed657fe21e7874bb7c5490d0724b435265fb105b
Benjamin Bolte <ben@bolte.cc>
2025-09-18 14:32:30 -0700

    script update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-
+# Check /sys/bus/usb/devices/*/power/control to make sure they are set to "on" instead of "auto"
+for device in /sys/bus/usb/devices/*/power/control; do
+    if [ "$(cat $device)" != "on" ]; then
+        if read -p "Set $device to on? (y/n) " -n 1 -r; then
+            echo "on" > $device
+        fi
+    fi
+done
