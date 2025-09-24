4ea78be8c73f0776aac06ae4ab3240f413bb81cd
Testing Kbot <dpsh@kbotv2-no11>
2025-09-24 15:01:15 -0700

    make deploy work with klog

--- firmware/deploy.sh
+++ firmware/deploy.sh
@@ -26,13 +26,9 @@ echo "Deploying policy: $policy"
 # set setup can interfaces and set max torques
 bash ~/kbot_deployment/scripts/reset_max_torques.sh
 
-run_firmware() {
-    sudo -E chrt 80 /home/dpsh/miniconda3/envs/klog/bin/python main.py $policy "${HOME}/kinfer-logs"
-}
+# run with klog
+# klog-deploy --no-wait "sudo -E chrt 80 /home/dpsh/miniconda3/envs/klog/bin/python main.py $policy"
 
-
-# run
-# klog-deploy --no-wait run_firmware "$policy"
-
-run_firmware "$policy"
+# run standalone
+sudo -E chrt 80 /home/dpsh/miniconda3/envs/klog/bin/python main.py "$policy" "${HOME}/kinfer-logs"
 
